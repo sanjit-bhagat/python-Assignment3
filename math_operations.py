b04d98b66e17a125763b7c023595abe876c59ed1
sanjit bhagat <sanjitbhagat632@gmail.com>
2026-01-15 00:29:40 +0530

    Add files via upload

---
+++ math_operations.py
@@ -0,0 +1,11 @@
+import math
+
+number=int(input("Enter a number:"))
+square_root=math.sqrt(number)
+logarithm=math.log(number)
+sine_value=math.sin(number)
+
+print("Square root of", number,"is:",square_root )
+print("Logarithm of", number,"is:",logarithm )
+print("Sine value of", number,"is:",sine_value )
+
